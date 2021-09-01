2e0a92527d3311e4b9eb70d839b7ef49424711af
vipultiwarihcl <82096164+vipultiwarihcl@users.noreply.github.com>
2021-09-01 19:17:42 +0530

    category page update

--- src/pages/category.js
+++ src/pages/category.js
@@ -4,6 +4,8 @@ import {
   getCookie,
 } from '../../helper/uiHelper'
 import { defineCustomElements } from 'design-web-components/loader'
+import styles from '../../styles/Home.module.css';
+import { products } from '../../mock/products'
 
 const {
   EditableArea,
@@ -30,15 +32,32 @@ function Category(props) {
 
   return (
     <div>
-      <h1>{title}</h1>
-      --- here
-      <ui-link link="http://google.com">
-        Design System component
-        </ui-link>
-      <ui-img src="https://www.shopwithmyrep.co.uk/mediamarket-uk/10038/strip-banner-causes-desktop_2_unq_d974c186c29743baa4aa7bc3320fcbcb.jpg" width="700" />
-        ---
-      <p> - {descText} {getCookie('Home')}</p>
-      {main && <EditableArea content={main} parentTemplateId={metadata['mgnl:template']} />}
+      <div className={styles.banner}>
+        <ui-img className={styles.banner_img} src="https://www.shopwithmyrep.co.uk/mediamarket-uk/10038/strip-banner-causes-desktop_2_unq_d974c186c29743baa4aa7bc3320fcbcb.jpg" width="900" />
+      </div>
+      <div>
+        <ui-heading label="Make-up" level="h1" />
+      </div>
+      <div className={styles.product_listing}>
+        {products.map((product, i) =>
+
+          <ui-product_card
+            key={i}
+            image={product.image}
+            imgwidth={340}
+            imgheight={product.imgheight}
+            imgalt={product.imgalt}
+            heading={product.heading}
+            btndisable={product.btndisable}
+            btnlabel={product.btnlabel}
+            price={product.price}
+            cardwidth={product.cardwidth}
+            cardheight={product.cardheight}
+            cardbgcolor={product.cardbgcolor}
+          />
+
+        )}
+      </div>
     </div>
   );
 }
