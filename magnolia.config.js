24c5971abe00d49ef6a24158d433adb5bb535f91
Dmitry Humeniuk <dmitry.humeniuk@hcl.com>
2021-10-28 23:04:30 +0200

    trying to reproduce hydration issue

--- magnolia.config.js
+++ magnolia.config.js
@@ -1,21 +1,32 @@
-import Category from './src/pages/category';
-import Home from './src/pages/Home';
-import Offer from './src/pages/offers';
-import Text from './src/components/Text';
-import { UiImg, UiTrendingBlock } from 'design-components'
+import React from "react";
+import Category from "./src/pages/category";
+import Home from "./src/pages/Home";
+import Offer from "./src/pages/offers";
+import Text from "./src/components/Text";
+import Image from "./src/components/Image";
+import List from "./src/components/List";
+import Item from "./src/components/Item";
+
+// import { ComponentWrapper } from './helper/ComponentWrapper'
+
+import { defineCustomElements } from "design-web-components/loader";
+
+const ComponentWrapper = (tag) => {
+  defineCustomElements();
+  return () => React.createElement(tag);
+};
 
 const config = {
-    componentMappings: {
-        // 'spa-lm:pages/Home': Home,
-        'spa-lm:pages/next-category': Category,
-        'spa-lm:pages/next-offer': Offer,
-        'spa-lm:pages/next-basic': Home,
+  componentMappings: {
+    // 'spa-lm:pages/Home': Home,
+    "spa-lm:pages/next-category": Category,
+    "spa-lm:pages/next-offer": Offer,
+    "spa-lm:pages/next-basic": Home,
 
-
-        'spa-lm:components/Text': Text,
-        'spa-lm:components/Image': UiImg,
-        'spa-lm:components/blockTrending': UiTrendingBlock,
-      },
+    "spa-lm:components/Text": Text,
+    "spa-lm:components/Image": ComponentWrapper("ui-img"),
+    "spa-lm:components/blockTrending": ComponentWrapper("ui-trending-block"),
+  },
 };
 
 export default config;
